2e950f35d0c0d1ec0bdcb3a38211c1b519c1322a
Simon Baslé <sbasle@vmware.com>
2023-04-27 18:06:20 +0200

    Trying different approach without Unit and externalizing parsing code

--- spring-context/src/test/java/org/springframework/format/datetime/standard/DateTimeFormattingTests.java
+++ spring-context/src/test/java/org/springframework/format/datetime/standard/DateTimeFormattingTests.java
@@ -31,6 +31,7 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -55,7 +56,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.format.annotation.DurationFormat;
 import org.springframework.format.annotation.DurationFormat.Style;
-import org.springframework.format.annotation.DurationFormat.Unit;
 import org.springframework.format.support.FormattingConversionService;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
@@ -725,7 +725,7 @@ class DateTimeFormattingTests {
 
 		private Duration duration;
 
-		@DurationFormat(style = Style.SIMPLE, defaultUnit = Unit.MICROS)
+		@DurationFormat(style = Style.SIMPLE, defaultUnit = ChronoUnit.MICROS)
 		private Duration styleDuration;
 
 		private Year year;
